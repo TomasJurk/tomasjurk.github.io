cefe6aa9d41f5b2d1a22219f65b4587ae55ccaf2
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2018-06-21 23:25:15 +0300

    Add files via upload

--- Portfolio/app/script.js
+++ Portfolio/app/script.js
@@ -3,19 +3,13 @@
 let moviesContainer = document.querySelector('.container');
 let cardLength;
 
-function scrollMovies(x) {
-    cardLength = document.querySelector('.card').offsetWidth + 16;
-    moviesContainer.scrollTo(cardLength*x, 0);
+
+function supportsFlexBox() {
+	var test = document.createElement('test');
+
+	test.style.display = 'flex';
+
+	return test.style.display === 'flex';
 }
 
-
-setInterval(scrollMovies(), 1000);
-
-
-/* for (let i = 0; i < 10; i+=2) {
-    if(i == 6) {
-        scrollMovies(400*i);
-        console.log('working');
-    }
-    
-} */
+// setInterval(scrollMovies(), 1000);
