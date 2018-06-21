b268cff64139ea757940b14520b3a95e68a623ec
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2018-06-21 13:22:35 +0300

    Half done

--- Portfolio/app/script.js
+++ Portfolio/app/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let moviesContainer = document.querySelector('.container');
+let cardLength;
+
+function scrollMovies(x) {
+    cardLength = document.querySelector('.card').offsetWidth + 16;
+    moviesContainer.scrollTo(cardLength*x, 0);
+}
+
+
+setInterval(scrollMovies(), 1000);
+
+
+/* for (let i = 0; i < 10; i+=2) {
+    if(i == 6) {
+        scrollMovies(400*i);
+        console.log('working');
+    }
+    
+} */
