262c6331c9ab3f53b74f3c21263364c530b3457b
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2018-06-24 11:28:17 +0300

    Add files via upload

--- Portfolio/app/script.js
+++ Portfolio/app/script.js
@@ -1,16 +1,60 @@
 'use strict';
+console.log('test');
+var moviesContainer = document.querySelector('.container');
 
+var genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
 
-let moviesContainer = document.querySelector('.container');
-let cardLength;
+var request = new XMLHttpRequest();
 
+request.open('GET', 'https://api.themoviedb.org/3/movie/top_rated?api_key=d3f09dc36b7dfd2241af1d67612a5679&language=en-US&page=1', true);
+request.onload = function () {
 
-function supportsFlexBox() {
-	var test = document.createElement('test');
+  var data = JSON.parse(this.response);
 
-	test.style.display = 'flex';
-
-	return test.style.display === 'flex';
+  if (request.status >= 200 && request.status < 400) {
+		console.log(data.results);
+		createEmelents(data.results);
+		console.log('Loaded');
+  } else {
+    console.log('error');
+  }
 }
 
-// setInterval(scrollMovies(), 1000);
+// get genre - genres[genres.findIndex(x => x.id == 10749)].name returns 'Romance'
+// Padaryti zhanru parodyma
+// padaryti auto slideri
+// uzbaigti stailus
+
+function createEmelents(movies) {
+	for (var i = 0; i < movies.length; i++) {
+		var newEl = document.createElement('div');
+		newEl.classList.add('card');
+		var newElContent = '<div class="card-header"><img src="http://image.tmdb.org/t/p/w300/' + movies[i].backdrop_path + 
+		'" alt="Movie image"><div class="card-header_right"><h3>' + movies[i].title + '</h3><p>genre, genre</p><button>more</button></div></div><div class="card-body"><div class="movie-rating"><img src="img/star.png" alt="Rating star"><h3>' + movies[i].vote_average + '</h3></div><div class="movie-descr"><p>' + movies[i].overview + '</p></div></div>';
+
+		// Zemiau kodas neveikia ant visu Internet Exlorer
+ 		/* var newElContent = `
+			<div class="card-header">
+				<img src="http://image.tmdb.org/t/p/w300/${movies[i].backdrop_path}" alt="Movie image">
+				<div class="card-header_right">
+					<h3>${movies[i].title}</h3>
+					<p>genre, genre</p>
+					<button>more</button>
+				</div>
+			</div>
+			<div class="card-body">
+				<div class="movie-rating">
+					<img src="img/star.png" alt="Rating star">
+					<h3>${movies[i].vote_average}</h3>
+				</div>
+				<div class="movie-descr">
+					<p>${movies[i].overview}</p>
+				</div>
+			</div>
+		`; */
+		newEl.innerHTML = newElContent;
+		moviesContainer.appendChild(newEl);
+	}
+}
+
+request.send();
