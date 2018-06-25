bc8c7e77e6e9c974ff38d42f477ac86feaaa9ad4
TomasJurk <33634105+TomasJurk@users.noreply.github.com>
2018-06-25 14:19:04 +0300

    Add files via upload

--- Portfolio/app/script.js
+++ Portfolio/app/script.js
@@ -68,11 +68,6 @@ if (!Array.prototype.filter) {
   };
 }
 
-/////////////////////////////// TODO ////////////////////////////////////////////////
-// Padaryti auto slideri
-// Uzbaigti stailus
-
-var mainCont = document.querySelector(".main");
 var moviesContainer = document.querySelector(".container");
 
 var genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}];
