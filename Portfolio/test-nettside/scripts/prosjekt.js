c4c5f311f05d80e172d9396804e1931a9c96f4e0
Tom <tomasstj@gmail.com>
2020-09-24 10:05:53 +0200

    .

---
+++ Portfolio/test-nettside/scripts/prosjekt.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const projectId = window.location.hash.split('#')[1];
+
+
+console.log(projectId);
+
+const projects = [
+    {
+        id: '',
+        title: '',
+        picture: '',
+        text: ''
+    }
+];
