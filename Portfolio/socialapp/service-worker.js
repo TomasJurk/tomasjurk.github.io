bd7135b07bf32608cc0210c2bb060f2495f85cc4
Tom <tomasstj@gmail.com>
2021-05-19 20:54:06 +0200

    .

--- Portfolio/socialapp/service-worker.js
+++ Portfolio/socialapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51c45e8c7660d16ae5a390d63284a0e9.js"
+  "https://tomasjurk.github.io/Portfolio/socialapp/precache-manifest.1b42ac1f82fe8bf98a7695aa33ad5d04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tomasjurk.github.io/Portfolio/socialapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
